0e9b2447e5d916928c0473a0fa606803a7947b64
Esko Takku <esko.takku@gmail.com>
2024-04-14 20:22:13 +0300

    trying to read data

---
+++ code/model_generator/dataset.py
@@ -0,0 +1,160 @@
+import os
+from typing import List
+from torchvision.io import read_image
+from torchvision.io import ImageReadMode
+from torchvision.transforms import v2
+import torchvision.transforms as transforms
+import torch
+import numpy
+from PIL import Image
+import cv2
+
+class ImageItem():
+    movable_labels = {
+        0: 0, # 'unlabeled'
+        1: 0, # 'ego vehicle'
+        2: 0, # 'rectification border'
+        3: 0, # 'out of roi' 
+        4: 0, # 'static'  
+        5: 0, # 'dynamic' 
+        6: 0, # 'ground'  
+        7: 0, # 'road' 
+        8: 0, # 'sidewalk'
+        9: 0, # 'parking' 
+        10: 0, # 'rail track' 
+        11: 0, # 'building'
+        12: 0, # 'wall' 
+        13: 0, # 'fence'
+        14: 0, # 'guard rail'
+        15: 0, # 'bridge'
+        16: 0, # 'tunnel'
+        17: 0, # 'pole'
+        18: 0, # 'polegroup'
+        19: 0, # 'traffic light'
+        20: 0, # 'traffic sign'
+        21: 0, # 'vegetation'
+        22: 0, # 'terrain'
+        23: 0, # 'sky'
+        24: 1, # 'person'
+        25: 1, # 'rider'
+        26: 1 , # 'car'
+        27: 1, # 'truck'
+        28: 1 , # 'bus'
+        29: 1 , # 'caravan'
+        30: 1 , # 'trailer'
+        31: 1 , # 'train'
+        32: 1 , # 'motorcycle'
+        33: 1, # 'bicycle'
+        -1: 1 , # 'license plate'
+    }
+
+    def __init__(self, image, right_image, disparity_image, classification_image):
+        self._image: str = image
+        self._image_r: str = right_image
+        self._dipsarity:  str = disparity_image
+        self._classification:  str = classification_image
+
+        self.size: int = 512
+
+
+    def classification(self):
+        gt = read_image(self._classification, mode=ImageReadMode.UNCHANGED)
+
+        _gt = torch.zeros(size=(2, gt.shape[1], gt.shape[2]), dtype=torch.bool)
+        _gt[:,:,:] = torch.tensor(0, dtype=torch.bool)
+
+        for i, val in self.movable_labels.items():
+            Y, X = numpy.where(gt[0]==torch.tensor(i, dtype=torch.int8))
+            
+            if val == 1: # movable
+                _gt[1, Y, X] = torch.tensor(1, dtype=torch.bool)
+            else: # unmoving
+                _gt[0, Y, X] = torch.tensor(1, dtype=torch.bool)
+        
+        return v2.Resize(size=self.size, interpolation=v2.InterpolationMode.NEAREST)(_gt)
+
+    def classification_image(self):
+        _ti = self.classification()
+        ti = numpy.zeros(shape=(_ti.shape[1], _ti.shape[2], 3) , dtype=numpy.uint8)
+        Y, X = numpy.where(_ti[0]==1) # unmovable
+        ti[Y, X, :] = (40, 209, 31)
+        Y, X = numpy.where(_ti[1]==1) # movable
+        ti[Y, X, :] = (194, 17, 73)
+        target_image = Image.fromarray(ti, 'RGB')
+        transform = transforms.Compose([ 
+            transforms.PILToTensor() 
+        ]) 
+        img_tensor = transform(target_image) 
+        return img_tensor
+
+    def left(self):
+        return v2.Resize(size=self.size)(read_image(self._image, mode=ImageReadMode.RGB))
+    
+    def right(self):
+        return v2.Resize(size=self.size)(read_image(self._image, mode=ImageReadMode.RGB))
+
+    def disparity(self):
+        image = Image.open(self._dipsarity).convert('RGB')
+
+        trnsfrms = transforms.Compose([transforms.Resize(self.size), 
+                        transforms.ToTensor()])
+        img_tensor = trnsfrms(image)
+
+        # print(self._dipsarity)
+        # img = cv2.imread(self._dipsarity, cv2.IMREAD_GRAYSCALE)
+        # img = read_image(self._dipsarity, mode=ImageReadMode.UNCHANGED)
+        return img_tensor # v2.Resize(size=self.size, interpolation=v2.InterpolationMode.NEAREST)(img)
+    
+
+class CityScapes():
+
+    def __init__(self, dataset_dir):
+        self.dataset_dir = dataset_dir
+
+        left_image_path = os.path.join(self.dataset_dir, "leftImg8bit_trainvaltest", "leftImg8bit", "train")
+        right_image_path = os.path.join(self.dataset_dir, "rightImg8bit_trainvaltest", "rightImg8bit", "train")
+        disp_truth_path = os.path.join(self.dataset_dir, "disparity_trainvaltest", "disparity", "train")
+        segm_truth_path = os.path.join(self.dataset_dir, "gtFine_trainvaltest", "gtFine", "train")
+
+        self.images: List[ImageItem] = []
+
+        # loop images
+        for city in os.listdir(left_image_path):
+            city_dir = os.path.join(left_image_path, city)
+            if os.path.isdir(city_dir):
+                # print(city)
+                for l_image in os.listdir(city_dir):
+                    # print(l_image)
+                    if l_image.endswith(".png"):
+                        # Right image path
+                        l_i_p = os.path.join(city_dir, l_image)
+                        if not os.path.isfile(l_i_p):
+                            raise Exception(f"Left image path {l_i_p} not found")
+
+                        # Left image path
+                        r_i_p = os.path.join(right_image_path, city, l_image.replace("leftImg8bit", "rightImg8bit"))
+                        if not os.path.isfile(r_i_p):
+                            raise Exception(f"Right image path {r_i_p} not found")
+
+                        # Disparity image path
+                        d_i_p = os.path.join(disp_truth_path, city, l_image.replace("leftImg8bit", "disparity"))
+                        if not os.path.isfile(d_i_p):
+                            raise Exception(f"Disparity image path {d_i_p} not found")
+
+                        # Classification image path
+                        c_i_p = os.path.join(segm_truth_path, city, l_image.replace("leftImg8bit", "gtFine_labelIds"))
+                        if not os.path.isfile(c_i_p):
+                            raise Exception(f"Classification image path {c_i_p} not found")
+
+                        image = ImageItem(
+                            l_i_p,
+                            r_i_p,
+                            d_i_p,
+                            c_i_p
+                            )
+                        
+                        self.images.append(image)
+
+    def __len__(self):
+        return len(self.images)
+
---
+++ code/model_generator/generate.py
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+from dataset import CityScapes
+from typing import TYPE_CHECKING
+from matplotlib import pyplot as plt
+from torchvision.utils import make_grid
+import torchvision.transforms.functional as F
+
+import numpy
+
+# Due to my laziness in setting up a vscode environment
+if TYPE_CHECKING:
+    from .dataset import CityScapes, ImageItem
+
+def show(imgs):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = F.to_pil_image(img)
+        axs[0, i].imshow(numpy.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+
+
+# Get images into a dataset
+    # image should have original
+    # Area of movable objects
+    # Disparity map
+dataset: "CityScapes" = CityScapes("/home/esko/Documents/Dippatyo/dataset")
+
+
+# Go trough images
+    # Go trough images 
+    # Remove areas with movable
+    # assume area same as around it
+    # Ask image to be approved
+    # save if approved
+for image in dataset.images:
+    # print(image._dipsarity)
+    # image.disparity(), image.classification()
+    grid = make_grid([image.left(), image.classification_image(), image.disparity()])
+    show(grid)
+
+    plt.show()
+    raise Exception("STOP")
